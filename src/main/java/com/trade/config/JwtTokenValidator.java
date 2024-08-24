c0ab86a3173e0a779cb39fa8e5bbaf203412116e
Ashok <ashokdhiman21308@gmail.com>
2024-08-24 16:02:21 +0530

    random commit

--- src/main/java/com/trade/config/JwtTokenValidator.java
+++ src/main/java/com/trade/config/JwtTokenValidator.java
@@ -1,12 +1,22 @@
 package com.trade.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.util.List;
 
 public class JwtTokenValidator extends OncePerRequestFilter {
 
@@ -15,6 +25,22 @@ public class JwtTokenValidator extends OncePerRequestFilter {
         String jwt=request.getHeader(JwtConstants.JWT_HEADER);
         if(jwt!=null){
             jwt=jwt.substring(7);
+
+            try{
+                SecretKey key= Keys.hmacShaKeyFor(JwtConstants.SECRET_KEY.getBytes());
+
+                Claims claims = Jwts.parser().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+                String email=String.valueOf(claims.get("email"));
+                String authorities=String.valueOf(claims.get("authorities"));
+                List<GrantedAuthority> authoritiesList= AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+                Authentication auth=new UsernamePasswordAuthenticationToken(email,authoritiesList,authoritiesList);
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }catch (Exception e){
+                throw new RuntimeException("Invalid Token...");
+            }
         }
+        filterChain.doFilter(request,response);
     }
 }
